5a52eefa762ce2c2acef7aa55606a36e44270742
Harsha Vardhan Reddy <acloudfacile@pm.me>
2025-07-25 12:02:21 +0900

    Update sftp_connection_check.py

--- lambda/sftp_connection_check.py
+++ lambda/sftp_connection_check.py
@@ -1,6 +1,5 @@
 import os
 import boto3
-import base64
 import paramiko
 import json
 
@@ -8,26 +7,23 @@ def get_secret(secret_name, region="ap-northeast-1"):
     session = boto3.session.Session()
     client = session.client(service_name="secretsmanager", region_name=region)
     get_secret_value_response = client.get_secret_value(SecretId=secret_name)
-    secret = get_secret_value_response["SecretString"]
-    return json.loads(secret)
+    return json.loads(get_secret_value_response["SecretString"])
 
 def lambda_handler(event, context):
     secret_name = os.environ.get("SECRET_NAME", "iota-sftp-creds")
     region = os.environ.get("AWS_REGION", "ap-northeast-1")
     creds = get_secret(secret_name, region)
-
+    
     host = creds["host"]
     username = creds["username"]
     password = creds["password"]
-
     port = 22
 
     try:
-        # Connect to SFTP using Paramiko (password-based)
         transport = paramiko.Transport((host, port))
         transport.connect(username=username, password=password)
         sftp = paramiko.SFTPClient.from_transport(transport)
-        print("✅ SFTP connection successful!")
+        print("✅ SFTP connection successful")
         sftp.close()
         transport.close()
         return {"status": "success", "msg": "SFTP connection successful"}
